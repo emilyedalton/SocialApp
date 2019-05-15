04310b4aeff9018caf912f227ae92f77ca1a9463
Emily Dalton <emilydalton@dhcp-10-105-33-155.wireless.northwestern.private>
2019-05-15 10:46:08 -0500

    adding lots of new components

---
+++ src/components/SettingsNav/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+
+class SettingsNav extends Component {
+    render () {
+    return(
+         <h1>Settings Nav</h1>    
+    )
+    
+    }
+    
+    
+    }
+    export default SettingsNav 
