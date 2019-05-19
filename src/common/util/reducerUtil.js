cf98a161ae7e5f54da24f0d89180846744048845
Emily Dalton <emilyedalton@gmail.com>
2019-05-19 08:46:52 -0500

    getting commit to show

--- src/common/util/reducerUtil.js
+++ src/common/util/reducerUtil.js
@@ -3,7 +3,8 @@ export const createReducer = (intialState, fnMap ) =>{
     return (state = intialState, {type, payload}) =>{
         const handler = fnMap[type];
 
-        return handler ? handler(state,payload): state
+        return handler ?
+         handler(state,payload): state
     }
 
 
