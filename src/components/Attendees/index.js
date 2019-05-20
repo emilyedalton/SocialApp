539cb9b186beb6d45fe6b31bad2891ba71378bdf
Emily Dalton <emilyedalton@gmail.com>
2019-05-20 11:59:51 -0500

    adding attendees sidebar component

--- src/components/Attendees/index.js
+++ src/components/Attendees/index.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { Segment, List, Item, Label} from 'semantic-ui-react'
+
 
 // import { Grid, Button} from 'semantic-ui-react'
 // import { connect } from 'react-redux'
@@ -9,13 +11,42 @@ import React, {Component} from 'react'
 
 
 class Attendees extends Component {
+   
 render (){
     return(
-<div>
-<h1> Attendees (Sidebar)</h1>
-</div>
-    )
-}
-
+     <div>
+        <Segment
+          textAlign="center"
+          style={{ border: 'none' }}
+          attached="top"
+          secondary
+          inverted
+          color="teal"
+        >
+          2 People Going
+        </Segment>
+        <Segment attached>
+          <List relaxed divided>
+            <Item style={{ position: 'relative' }}>
+              <Label
+                style={{ position: 'absolute' }}
+                color="orange"
+                ribbon="right"
+              >
+                Host
+              </Label>
+              <Item.Image size="tiny" src="/assets/user.png" />
+              <Item.Content verticalAlign="middle">
+                <Item.Header as="h3">
+                  <a>Attendee Name</a>
+                </Item.Header>
+              </Item.Content>
+            </Item>
+          </List>
+        </Segment>
+      </div>
+    
+)
+    }
     }
     export default Attendees
