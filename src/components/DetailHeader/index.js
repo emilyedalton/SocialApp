a4f90ed132d68c9cd55818d7c79c35514c9d9855
Emily Dalton <emilyedalton@gmail.com>
2019-05-20 11:57:12 -0500

    adding stateless functionl versions of event info and comments

--- src/components/DetailHeader/index.js
+++ src/components/DetailHeader/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-// import { Grid, Button} from 'semantic-ui-react'
+import { Segment, Image, Item, Header, Button} from 'semantic-ui-react'
 // import { connect } from 'react-redux'
 // import EventList from '../EventList'
 // import EventForm from '../EventForm';
@@ -10,9 +10,45 @@ import React, {Component} from 'react'
 class DetailHeader extends Component {
 render (){
     return(
-<div>
-    <h1>Header</h1>
-</div>
+        <div>
+   <Segment.Group>
+      <Segment basic attached="top" style={{ padding: '0', background: "Purple" }}
+      
+      >
+        {/* <Image src="/assets/categoryImages/drinks.jpg" fluid /> */}
+
+        <Segment basic>
+          <Item.Group>
+            <Item>
+              <Item.Content>
+                <Header
+                  size="huge"
+                  content="Content"
+                  style={{ 
+                    
+                    color: 'White' 
+                }}
+                />
+                <p>Event Date</p>
+                <p>
+                  Hosted by <strong>Hosted by</strong>
+                </p>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Segment>
+      </Segment>
+
+      <Segment attached="bottom">
+        <Button>Cancel My Place</Button>
+        <Button color="teal">JOIN THIS EVENT</Button>
+
+        <Button color="orange" floated="right">
+          Manage Event
+        </Button>
+      </Segment>
+    </Segment.Group>
+    </div>
     )
 }
 
