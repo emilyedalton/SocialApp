5ee5e9e6985cd74051723cbde7c13e7967ea9dd5
Emily Dalton <emilyedalton@gmail.com>
2019-05-20 10:15:47 -0500

    adding skeleton components for event details

---
+++ src/components/Attendees/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+// import { Grid, Button} from 'semantic-ui-react'
+// import { connect } from 'react-redux'
+// import EventList from '../EventList'
+// import EventForm from '../EventForm';
+// import cuid from 'cuid'
+// import {createEvent, deleteEvent, updateEvent} from '../EventList/eventActions'
+
+
+class Attendees extends Component {
+render (){
+    return(
+<div>
+    
+</div>
+    )
+}
+
+    }
+    export default Attendees
