6376a48ebcee585c64507fce0e569aa689f895dc
Emily Dalton <emilyedalton@gmail.com>
2019-05-22 13:17:08 -0500

    removing sidebar form

--- src/components/EventDashboard/index.js
+++ src/components/EventDashboard/index.js
@@ -11,72 +11,72 @@ const mapState =(state) => ({
 })
 
 const actions = {
-    createEvent,
+    // createEvent,
     deleteEvent,
-    updateEvent
+    // updateEvent
 }
 class EventDashboard extends Component {
 //can remove the consturctor when you use arrow functions on the methods. 
     // constructor(props){
     //     super(props)
- state = {
-    isOpen: false,
-    selectedEvent: null
+//  state = {
+//     isOpen: false,
+//     selectedEvent: null
 
-}
+// }
 //aarow functin in the method does the same thing as below
 // this.handleFormOpen = this.handleFormOpen.bind(this);
 // this.handleCancel = this.handleCancel.bind(this);
 
 
 
-    handleFormOpen =()=>{
-        this.setState({
-            isOpen: true,
-        })
+    // handleFormOpen =()=>{
+    //     this.setState({
+    //         isOpen: true,
+    //     })
     
-    }
+    // }
 
-    handleCancel=()=>{
-        this.setState({
-            isOpen: false,
-            selectedEvent: null
-        })
+    // handleCancel=()=>{
+    //     this.setState({
+    //         isOpen: false,
+    //         selectedEvent: null
+    //     })
     
-    }
-    handleCreateEvent = (newEvent) =>{
-        newEvent.id= cuid();
-        newEvent.PhotoUrl= "photo shoud be here";
-        this.props.createEvent(newEvent)
-        this.setState({
-            isOpen: true
-        })
+    // }
+    // handleCreateEvent = (newEvent) =>{
+    //     newEvent.id= cuid();
+    //     newEvent.PhotoUrl= "photo shoud be here";
+    //     this.props.createEvent(newEvent)
+    //     this.setState({
+    //         isOpen: true
+    //     })
 
 
-    }
+    // }
 
     handleDeleteEvent = (eventID)=>() => {
         this.props.deleteEvent(eventID)
 
         }
     
-    handleOpenEvent=(eventToOpen)=>() =>{
-        this.setState({
-            selectedEvent: eventToOpen,
-            isOpen: true
+//     handleOpenEvent=(eventToOpen)=>() =>{
+//         this.setState({
+//             selectedEvent: eventToOpen,
+//             isOpen: true
 
-        })
-    }
-handleUpdateEvent = (updatedEvent)=>{
-    this.props.updateEvent(updatedEvent)
-    this.setState({
-        isOpen: false, 
-        selectedEvent: null
-    })
+//         })
+//     }
+// handleUpdateEvent = (updatedEvent)=>{
+//     this.props.updateEvent(updatedEvent)
+//     this.setState({
+//         isOpen: false, 
+//         selectedEvent: null
+//     })
 
-}
+// }
     render () {
-        const {selectedEvent} =this.state
+        // const {selectedEvent} =this.state
         const {events} =this.props
     return(
     
@@ -84,13 +84,13 @@ handleUpdateEvent = (updatedEvent)=>{
 <Grid.Column width ={10}>
 
 <EventList
-onEventOpen={this.handleOpenEvent}
+// onEventOpen={this.handleOpenEvent}
 events={events}
 deleteEvent ={this.handleDeleteEvent}
 />
 </Grid.Column>
 <Grid.Column width ={6}>
-<Button
+{/* <Button
 onClick = {this.handleFormOpen}
 positive content ="Create Event"/>
 {this.state.isOpen &&
@@ -98,9 +98,9 @@ positive content ="Create Event"/>
 handleCancel={this.handleCancel}
 createEvent={this.handleCreateEvent}
 selectedEvent={selectedEvent}
-updateEvent ={this.handleUpdateEvent}
+updateEvent ={this.handleUpdateEvent} */}
 
-/>}
+
 </Grid.Column>
 
 
