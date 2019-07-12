6e85aa685030e8334360b518005f507abe5a0302
Emily Dalton <emilyedalton@gmail.com>
2019-07-12 09:00:20 -0500

    config

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -6,7 +6,8 @@ class AboutPage extends Component {
     render () {
     return(
         
-         <h1>About Page</h1>    
+         <h1>About Page</h1>   
+         <h2>more info</h2> 
     )
     
     }
