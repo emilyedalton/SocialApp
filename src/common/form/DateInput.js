84856e165b994ea7070b70b0154cc8b7bf9de875
Emily Dalton <emilyedalton@gmail.com>
2019-07-18 13:36:02 -0500

    date consistiency

--- src/common/form/DateInput.js
+++ src/common/form/DateInput.js
@@ -4,14 +4,28 @@ import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
 
-const DateInput =({input, width, placeholder, meta:{touched, error},...rest})=>{
+const DateInput =({input:{
+value,
+onChange,
+onBlur
+
+}, 
+width, 
+placeholder, 
+meta:{touched, error},...rest})=>{
     return(
 <Form.Field error={touched && !!error}>
 <DatePicker
 {...rest}
 placeholerText={placeholder}
-selected={input.value ? new Date (input.value): null}
-onChange={input.onChange}
+selected={value 
+    ? (Object.prototype.toString.call(value) !== '[object Date]') 
+    ? value.toDate(): value 
+    : null
+}
+onChange={onChange}
+onBlur={(e, val)=> onBlur(val)}
+onChangeRaw={(e)=> e.preventDefault}
 />
 {touched && error && <Label basic color='red'>{error}</Label>}
 </Form.Field>
