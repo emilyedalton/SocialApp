c6461ec29bc16b6d2e2db6d060939331a25d9af0
Emily Dalton <emilyedalton@gmail.com>
2019-08-15 15:42:29 -0500

    adding more streamlined home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,30 @@
 import React from 'react'
 
-import {Button, Container, Header} from 'semantic-ui-react'
+import {Button, Container, Header, Grid} from 'semantic-ui-react'
 import  './style.css'
 
 const Home =({history})=> {
     return(
         <div>
-
+       <Grid centered columns={1} style={{height: "100vh"}}  >
+  
         <Container 
-       
-        
+        style = {{margin: "auto"}}
         >
-       
+
          <Header as='h1' content="Northwestern University Press | Author Portal" 
          style={{
-             fontSize: '4em',
-             marginTop: "3em",
              color: "#4E2A84"
+         
          }}
          />  
          <Button onClick={()=>history.push('/welcome')}
-         basic content="Get Started"
-         />
+         basic content="Get Started"/>
+         
+         
          </Container>
+         </Grid>
+
          </div>
     )
     
