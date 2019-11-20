8c3ef9d7711f6b2262da4e72b3771b431cf95ed4
Emily Dalton <emilyedalton@gmail.com>
2019-11-20 13:15:43 -0600

    reset modal

---
+++ src/components/Reset/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Form, Segment, Button, Label } from 'semantic-ui-react';
+import {connect} from 'react-redux'
+import { Field, reduxForm } from 'redux-form';
+import TextInput from '../../common/form/TextInput';
+import { resetPassword} from '../../common/auth/authActions'
+
+const actions = {
+    
+    resetPassword
+}
+
+const ResetForm = ({error, resetPassword, handleSubmit}) => {
+  return (
+    <Form  size="large" onSubmit={(handleSubmit(resetPassword))}>
+      <Segment>
+        <Field
+          name="email"
+          component={TextInput}
+          type="text"
+          placeholder="Email Address"
+        />
+        <Field
+          name="password"
+          component={TextInput}
+          type="password"
+          placeholder="password"
+        />
+        {error && <Label basic color='red'>{error}</Label>}
+        <Button fluid size="large" color="teal">
+          Login
+        </Button>
+      </Segment>
+    </Form>
+  );
+};
+
+export default connect(null, actions)(reduxForm({form: 'resetForm'}) (ResetForm));
